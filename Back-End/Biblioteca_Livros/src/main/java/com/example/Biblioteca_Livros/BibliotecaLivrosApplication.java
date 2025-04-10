84480bdc8ce751130040049a31e7d74b99b930b8
alexsant22 <alexandre.santana2201@gmail.com>
2025-04-10 14:14:03 -0300

    Projeto Criado

---
+++ Back-End/Biblioteca_Livros/src/main/java/com/example/Biblioteca_Livros/BibliotecaLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.example.Biblioteca_Livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaLivrosApplication.class, args);
+	}
+
+}
---
+++ Back-End/Biblioteca_Livros/src/test/java/com/example/Biblioteca_Livros/BibliotecaLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Biblioteca_Livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
